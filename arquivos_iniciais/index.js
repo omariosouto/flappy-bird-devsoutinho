3969a225941c9fa3e30b9662e4fc034ee2b257ff
Mario Souto <soutomarios@gmail.com>
2020-04-09 20:03:24 -0300

    arquivos iniciais do jogo

---
+++ arquivos_iniciais/index.js
@@ -0,0 +1,8 @@
+console.log('[DevSoutinho] Flappy Bird');
+console.log('Inscreva-se no canal :D https://www.youtube.com/channel/UCzR2u5RWXWjUh7CwLSvbitA');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
